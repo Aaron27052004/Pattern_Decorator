0591c4eadcd797dbb5aab14878bc372fe17ddca5
Aaron <aaron.khayat@gmail.com>
2026-05-10 12:18:26 +0200

    modif du main pour test

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
-public class Main
-{
+
+public class Main{
+    
     public static void runApplication() {
         JFrame frame= new JFrame("Decorated shapes");
         MyShapePanel panel= new MyShapePanel();
@@ -11,7 +12,7 @@ public class Main
         MyShape shape0= new MyBorderDecorator(5, Color.red,
                             new MyRectangle (Color.green,
                                 new Point( 20, 20), new Point(120, 60) ));
-                                
+
         MyShape shape1= new MyCenterDecorator(7, Color.black,
                             new MyRectangle (Color.cyan,
                                 new Point(150, 90), new Point(250, 30) ));
@@ -23,7 +24,8 @@ public class Main
                                         new Point(200, 120), new Point(100, 100),
                                             new Point( 50, 150) )));
         
-        panel.add(shape0); panel.add(shape1); panel.add(shape2);
+        MyShape point = new MyBorderDecorator(50, Color.black ,new MyPolygon(Color.black,new Point (100,100)));
+        panel.add(shape0); panel.add(shape1); panel.add(shape2); panel.add(point);
     }
 
     public static void main(String args[]) {
